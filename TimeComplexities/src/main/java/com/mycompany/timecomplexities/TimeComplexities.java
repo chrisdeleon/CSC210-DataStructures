5ac59e59c79fa6db56bcd096ebf71542f218b33b
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2023-02-12 07:38:31 -0600

    Add files via upload

---
+++ TimeComplexities/src/main/java/com/mycompany/timecomplexities/TimeComplexities.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timecomplexities;
+
+/**
+ * Chris de Leon
+ * CSC210 - Time Complexities
+ * 2/12/2023
+ */
+public class TimeComplexities {
+
+    public static void main(String[] args) {
+        // main method
+        // instantiate the AsymptoticAnalysis class
+        // Create an array with 100,000 numbers
+        // Assign the array to the objects array property
+        // Using the object's array, invoke each class method and display appropriate results for each
+        
+        
+        
+    }
+    
+    public class AsymptoticAnalysis {
+        private int[] Array;
+        
+        // this returns the array created from the main method and stored in the private variable
+        public int[] getArray(){
+            return Array;
+        }
+        // this sets the array created in the main method
+        public void setArray (int[] Array){
+            this.Array = Array;
+        }
+        // method 1 - linear
+        // display all the elements in the array
+        // and the number of steps the algorithm completed
+        
+        // method 2 - constant
+        // return the number of steps the algorithm completed
+        
+        // method 3 - logarithm
+        // return the number of iterations
+        
+        // method 4 - quadratic
+        // return the number of iterations
+    }
+            
+            
+}
